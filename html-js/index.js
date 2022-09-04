e80856bbf97f3a9f137d7138c97e15add1d83fd1
Daniel Pinto <apintod@uninorte.edu.co>
2022-09-04 01:44:45 -0500

    leyendo html con js

---
+++ html-js/index.js
@@ -0,0 +1,22 @@
+
+let h1 = document.querySelector('h1'); // Por etiquetas
+
+let p = document.querySelector('p'); // "
+
+let dosParrafos = document.querySelector('.dosParrafos');
+
+let tresParrafos = document.querySelector('#tresParrafos');
+
+let input = document.querySelector('input');
+
+console.log({
+
+    h1,
+    p,
+    dosParrafos,
+    tresParrafos,
+    input
+
+});
+
+console.log(h1);
