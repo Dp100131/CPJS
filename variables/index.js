fb48ae04472a7a8f1fb4da7158ec2ca2767cfa74
Daniel Pinto <apintod@uninorte.edu.co>
2022-09-04 00:27:42 -0500

    finish variable

---
+++ variables/index.js
@@ -0,0 +1,33 @@
+
+/**
+ *  Notas: Variables:
+ * 
+ *      var name_variable // menos usada
+ *      let name_varible // usada
+ *      const name_variable // constantes
+ * 
+ * */ 
+
+let name = 'Daniel'
+
+name = 'Saidy' // Cambia
+
+const LASTNAME = 'Pinto'
+
+/* LASTNAME = 'Saidy' */ // Error es una constante por lo tanto no se puede cambiar el nombre
+
+let lastname // declarar
+
+lastname = 'pinto' //inicializar
+
+lastname = 'Ortega' //Reinicializar
+
+let suma = 2 + 2 // 4
+
+let sumaString = 'Hola, ' + 'Como estas?' // Hola, Como estas?
+
+let condition = 2 > 3 // bool false
+
+console.log(condition)
+
+// Se puede ponder ; o no
